f9ba21780960aa346f6705cadc37a415dbb9dad7
yahyabendahan <bendahan77@gmail.com>
2023-04-25 14:05:44 +0000

    test

--- spring-batch-example-master/src/main/java/com/eai/BatchJobCDL/writer/WriterImpayesCDL.java
+++ spring-batch-example-master/src/main/java/com/eai/BatchJobCDL/writer/WriterImpayesCDL.java
@@ -14,7 +14,6 @@ import com.eai.BatchJobCDL.repository.ImpayesCDLRepository;
 @Component
 public class WriterImpayesCDL extends JdbcBatchItemWriter<ImpayesCDLModel>{
 
-	//test
 	
 	    //@Autowired
 	    public WriterImpayesCDL(DataSource dataSource) {
@@ -23,7 +22,8 @@ public class WriterImpayesCDL extends JdbcBatchItemWriter<ImpayesCDLModel>{
 	        String deleteSql = "DELETE FROM IMPAYES_CDL";
 	        new JdbcTemplate(dataSource).update(deleteSql);
 	        
-	        
+	    	//test
+
 	        this.setSql("INSERT INTO IMPAYES_CDL (NATENG, TYPE, CPT, MONTANT_CREANCE, DATE_CREANCE, NO_DOSSIER, DATE_ECHEANCE, DATE_MISE_IMPAYE,DATE_REGLEMENT, MONTANT_AMORTISSEMENT, MONTANT_INTERET_NORMAL, TVA_INTERET, MONTANT_INTERET_RETARD, TVA_INTERET_RETARD, MONATANT_PENALITE_RETARD, TVA_PENALITE_RETARD, NUM_COMPTE_PAYEUR, REFFERENCE_VALEUR, REFERNCE_LIAISON, COMISSION, CODE_REJET, CODE_ETABLISST_BANCAIRE, CODE_GUICHET_BANCAIRE, DATE_REMISE, ENCOURS_EES_CONSO, ENCOURS_EES_COMM, ENCOURS_ESB, ENCOURS_ESI ,SOLDE, ENCOURS_DECLASSE, ENCOURS_RISQUE, TYPE_CLIENT, NUM_TIERS, CODE_CATEGORIE, NUM_DOSSIER_COMPLET, NUMERO_LIGNE, NUMERO_TIRAGE) " +
 	                "VALUES (:nateng, :type, :cpt, :montantCreance, :dateCreance, :noDossier, :dateEcheance, :dateMiseImpaye, :dateReglement, :montantAmortissement, :montantInteretNormal, :tvaInteret, :montantInteretRetard, :tvaInteretRetard, :montantPenaliteRetard, :tvaPenaliteRetard, :numComptePayeur, :referenceValeur, :referenceLiaison, :commission, :codeRejet, :codeEtablissementBancaire, :codeGuichetBancaire, :dateRemise, :encoursEesConso, :encoursEesComm, :encoursEsb, :encoursEsi, :solde, :encoursDeclasse, :encoursRisque, :typeClient, :numTiers, :codeCategorie, :numDossierComplet, :numeroLigne, :numeroTirage)");
 	     //  repository.save(null)
