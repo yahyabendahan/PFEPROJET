53fbce0cdf1ec8e465cdbb64c4271c712179d82e
yahyabendahan <bendahan77@gmail.com>
2023-04-28 10:21:30 +0000

    11:21 28/04/23

--- spring-batch-example-master/src/main/java/com/eai/BatchJobCDL/readerTEST/itemReaderTC.java
+++ spring-batch-example-master/src/main/java/com/eai/BatchJobCDL/readerTEST/itemReaderTC.java
@@ -3,20 +3,122 @@ package com.eai.BatchJobCDL.readerTEST;
 import javax.sql.DataSource;
 
 import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 
+import com.eai.BatchJobCDL.DTO.DocDTO;
 import com.eai.BatchJobCDL.model.ImpayesCDLModel;
+import com.eai.BatchJobCDL.utils.Constantes;
 
-
-public class itemReaderTC extends JdbcCursorItemReader<ImpayesCDLModel> {
+/*
+ * 
+ * @Value("classpath*:/data/customer*.csv")
+	@Bean
+	public MultiResourceItemReader<Customer> multiResourceItemreader() {
 	
-		public  itemReaderTC(DataSource dataSource) {
-	    JdbcCursorItemReader<ImpayesCDLModel> reader = new JdbcCursorItemReader<>();
-	    reader.setDataSource(dataSource);
-	    reader.setName("ItemReaderJustForShow");
-	    reader.setSql("SELECT * FROM IMPAYES_CDL");
-	    reader.setRowMapper(new BeanPropertyRowMapper<>(ImpayesCDLModel.class));
-	}
+		    String filePath = Constantes.FILE_PATH;
+
+	
+	
+		MultiResourceItemReader<Customer> reader = new MultiResourceItemReader<>();
 		
+		
+		reader.setDelegate(customerItemReader()); //
+		
+		reader.setResource(new FileSystemResource(filePath));
+		
+		return reader;
+
+	}*/
+
+
+/*
+ * @Override
+protected ItemReader<Foo> getItemReader() throws Exception {
+  MultiResourceItemReader<Foo> multiReader = new MultiResourceItemReader<>();
+  FlatFileItemReader<Foo> fileReader = new FlatFileItemReader<>();
+  fileReader.setLineMapper(new LineMapper<Foo>() {
+    @Override
+    public Foo mapLine(String line, int lineNumber) throws Exception {
+      Foo foo = new Foo();
+      foo.setValue(Integer.valueOf(line));
+      return foo;
+    }
+    
+  });
+  fileReader.setSaveState(true);
+  multiReader.setDelegate(fileReader);
+  Resource r1 = new ByteArrayResource("1\n2\n".getBytes());
+  Resource r2 = new ByteArrayResource("".getBytes());
+  Resource r3 = new ByteArrayResource("3\n".getBytes());
+  Resource r4 = new ByteArrayResource("4\n5\n".getBytes());
+  multiReader.setResources(new Resource[] { r1, r2, r3, r4 });
+  multiReader.setSaveState(true);
+  multiReader.setComparator(new Comparator<Resource>() {
+    @Override
+    public int compare(Resource arg0, Resource arg1) {
+      return 0; // preserve original ordering
+    }
+    
+  });
+  return multiReader;
+}*/
+
+
+/*
+ * @Bean
+public MultiResourceItemReader<Person> multiItemReader() {
+  ResourcePatternResolver patternResolver =
+      new PathMatchingResourcePatternResolver();
+  Resource[] resources = null;
+  try {
+    resources = patternResolver
+        .getResources("file:target/test-inputs/*.csv");
+  } catch (IOException e) {
+    LOGGER.error("error reading files", e);
+  }
+
+  return new MultiResourceItemReaderBuilder<Person>()
+      .name("multiPersonItemReader").delegate(itemReader())
+      .resources(resources).setStrict(true).build();
+}
+ */
+
+
+public class ReaderDto extends MultiResourceItemReader<DocDTO> {
+	//DOC
+	
+	private FlatFileItemReader<DocDTO> flatFileItemReader;
+
+	public ReaderDoc() {
+	    String filePath = Constantes.FILE_PATH;
+	    String[] columnNames = Constantes.DOC_ColumnNames;
+
+	    flatFileItemReader = new FlatFileItemReader<>();
+	    flatFileItemReader.setResource(new FileSystemResource(filePath));
+	    flatFileItemReader.setName("DOC-Reader");
+
+	    BeanWrapperFieldSetMapper<DocDTO> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
+	    DefaultLineMapper<DocDTO> defaultLineMapper = new DefaultLineMapper<>();
+	    DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
+
+	    lineTokenizer.setDelimiter("|");
+	    lineTokenizer.setStrict(false);
+	    lineTokenizer.setNames(columnNames);
+
+	    fieldSetMapper.setTargetType(DocDTO.class);
+
+	    defaultLineMapper.setLineTokenizer(lineTokenizer);
+	    defaultLineMapper.setFieldSetMapper(fieldSetMapper);
+
+	    flatFileItemReader.setLineMapper(defaultLineMapper);
+	    System.out.println("\nValider.itemReaderDoc\n");
+	}
+
+	
 }
 	
--- spring-batch-example-master/src/main/java/com/eai/BatchJobCDL/utils/Constantes.java
+++ spring-batch-example-master/src/main/java/com/eai/BatchJobCDL/utils/Constantes.java
@@ -11,6 +11,8 @@ public class Constantes {
 	public static final String MCN_FILE_PATH = "C:\\Users\\acer\\Desktop\\pfe\\fichier donnees\\CDL_MCN.creances";
 	public static final String DOC_FILE_PATH = "C:\\Users\\acer\\Desktop\\pfe\\fichier donnees\\CDL_DOC.creances";
 	public static final String EFA_FILE_PATH = "C:\\Users\\acer\\Desktop\\pfe\\fichier donnees\\CDL_EFA.creances";
+	public static final String FILE_PATH = "C:\\Users\\acer\\Desktop\\pfe\\fichier donnees\\CDL_*.creances";
+
 	public static final String[] ECH_ColumnNames = {"age", "nateng", "type", "cpt", "raisonSocial", "montantCreance", "dateCreance", "idClient", "noDossier", "dateEcheance", "dateMiseImpaye", "dateReglement", "montantAmortissement", "montantInteretNormal", "tvaInteret", "montantInteretRetard", "tvaInteretRetard", "montantPenaliteRetard", "tvaPenaliteRetard", "numComptePayeur", "codeCategorie", "numDossierComplet", "numeroLigne", "numeroTirage"};
 	public static final String[] SBF_ColumnNames = {"age", "nateng", "type", "cpt", "raisonSocial", "montantCreance", "dateCreance", "idClient", "noDossier", "refferenceValeur", "codeEtablissementBancaire", "codeGuichetBancaire", "dateRemise", "referenceLiaison", "codeRejet", "commission", "field16", "field17"};
 	public static final String[] ESC_ColumnNames = {"age", "nateng", "type", "cpt", "raisonSocial", "montantCreance", "dateCreance", "noDossier", "refferenceValeur", "dateEcheance", "codeEtablissementBancaire", "codeGuichetBancaire", "dateRemise", "referenceLiaison", "codeRejet", "commission", "field16", "field17"};
