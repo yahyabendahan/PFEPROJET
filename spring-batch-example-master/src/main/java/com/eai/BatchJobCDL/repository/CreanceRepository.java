d80d913790ccae125c9dca0c3e0ffb61f57a09e6
yahyabendahan <bendahan77@gmail.com>
2023-06-20 15:13:29 +0100

    20/06/2023

--- spring-batch-example-master/src/main/java/com/eai/BatchJobCDL/repository/CreanceRepository.java
+++ spring-batch-example-master/src/main/java/com/eai/BatchJobCDL/repository/CreanceRepository.java
@@ -1,14 +1,14 @@
-//package com.eai.BatchJobCDL.repository;
-//
-//import java.util.Date;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//import com.eai.BatchJobCDL.model.ImpayesCDLModel;
-//
-//@Repository
-//public interface CreanceRepository extends JpaRepository<ImpayesCDLModel, Long> {
+package com.eai.BatchJobCDL.repository;
+
+import java.util.Date;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.eai.BatchJobCDL.model.ImpayesCDLModel;
+
+@Repository
+public interface CreanceRepository extends JpaRepository<ImpayesCDLModel, Long> {
 //    ImpayesCDLModel findByCodeNatEngAndCodeDossierAndDateEcheanceAndNumeroLigneAndNumeroTirage(
 //            String nateng /*codeNatEng*/, String codeDossier /* noDossier !!*/, Date dateEcheance, int numeroLigne, int numeroTirage);
-//}
+}
